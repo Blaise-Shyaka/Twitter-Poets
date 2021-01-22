77a110caa1659540a9e287d779a37ed6e83c1bb1
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-22 20:16:40 +0200

    Solve the n+1 problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.includes(:tweets).find(params[:id])
     @followed_by = @user.followers.last(5)
   end
 
