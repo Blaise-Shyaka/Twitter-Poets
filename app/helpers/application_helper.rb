a6c28e9d658c542324c04886c7364ecc51f0d203
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-22 15:56:38 +0200

    display default avi or cover image if the user has none

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def display_navigation
     render '/layouts/navigation_bar' unless current_page?(new_user_path) || current_page?(new_session_path) || current_page?(root_path)
   end
+
+  def display_avi(image_source)
+    return image_tag '/assets/person' if image_source.nil?
+    image_tag "#{current_user.photo}"
+  end
+
+  def display_cover_photo(image_source)
+    return image_tag '/assets/cover-img' if image_source.nil?
+    image_tag "#{current_user.cover_image}"
+  end
 end
