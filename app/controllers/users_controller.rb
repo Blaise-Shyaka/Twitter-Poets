da52c91f15730497f8521f6d495e233ace657fb9
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-22 04:47:25 +0200

    add users controller and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.notice = 'Account created successfully'
+    else
+      render :new
+      flash.now[:notice] = 'Failed to create an account'
+    end
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :fullname)
+  end
+end 
