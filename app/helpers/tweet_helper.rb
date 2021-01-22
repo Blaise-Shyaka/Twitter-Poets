d1d91b87a3e1e387c0e0339bb13c988b5cf14e42
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-01-22 18:55:35 +0200

    fix bug in the user profile page view

--- app/helpers/tweet_helper.rb
+++ app/helpers/tweet_helper.rb
@@ -2,7 +2,6 @@ module TweetHelper
   def display_who_to_follow(list)
     markup = ''
     list.each do |flw|
-      p flw.username
       markup << "#{render 'a_follow', flw: flw}"
     end
     p markup
